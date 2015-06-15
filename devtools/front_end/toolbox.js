5ffe524d019161bcd7561e0d20b870690299a2a4
PhistucK <phistuck@users.noreply.github.com>
2015-06-15 09:07:01 +0300

    Test a bad change

--- devtools/front_end/toolbox.js
+++ devtools/front_end/toolbox.js
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-Runtime.startApplication("toolbox");
+Runtime.startApplication("toolbox"1);
 // Whitespace change
