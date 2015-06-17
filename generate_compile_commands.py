1f2bd1d3fe74c712c3323f992e6ba139c2b8e1bb
PhistucK <phistuck@users.noreply.github.com>
2015-06-17 09:13:55 +0300

    Made sure bad exit codes bubble to the task.

--- generate_compile_commands.py
+++ generate_compile_commands.py
@@ -10,6 +10,11 @@ for file_name in JSON_FILE_NAMES:
  for module in json.load(open(JSON_PATH + file_name)):
   modules.add(module["name"])
 output_script = open("./compile_frontend_separetely.sh", 'w+')
+output_script.write("final_code=0\n")
 for module in modules:
- output_script.write("echo Compiling " + module + "...\n" + COMMAND_LINE + module + "\n")
+ output_script.write("echo Compiling " + module + "...\n")
+ output_script.write(COMMAND_LINE + module + "\n")
+ output_script.write("current_code=$?; ")
+ output_script.write("if [[ $current_code != 0 ]]; then final_code=1; fi\n")
+output_script.write("exit $final_code")
 output_script.close()
