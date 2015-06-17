d424e1b953e4f62441acec97bbacaf1dc89e3793
PhistucK <phistuck@users.noreply.github.com>
2015-06-17 08:53:16 +0300

    Update toolbox.js

--- devtools/front_end/toolbox.js
+++ devtools/front_end/toolbox.js
@@ -2,5 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-Runtime.startApplication("toolbox"1);
+Runtime.startApplication("toolbox");
 // Whitespace change
